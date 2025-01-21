ca70328f8d5cc8efd281309ebcc31c987063f0b7
Aamir Sohail KmAs <AamirSohailKmAs@gmail.com>
2025-01-21 16:34:37 +0500

    feat: add pwa

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'sharpcoupon-pwa-v1';
+const urlsToCache = [
+    '/',
+    '/defaults/images/icons/sharp-coupon-192x192.png',
+    '/defaults/images/icons/sharp-coupon-512x512.png',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
